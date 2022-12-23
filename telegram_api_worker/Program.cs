e6eb90bcd95385dcf87abbc2a25b8631145e02bb
Andrey Matveev <andrewmat2000@gmail.com>
2022-12-23 16:41:31 +0500

    WIP

--- telegram_api_worker/Program.cs
+++
@@ -1,9 +0,0 @@
-using TelegramApiWorker;
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHostedService<BotService>();
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
